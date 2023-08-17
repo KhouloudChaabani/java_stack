c70ca6f97d5847a5f32f0cc8830200056e2a7924
KhouloudChaabani <khouloudchaabani33@gmail.com>
2023-08-17 22:13:49 +0100

    clinica

---
+++ oop/core/clinicapp/AdminUser.java
@@ -0,0 +1,83 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AdminUser extends User implements  HIPAACompliantUser ,HIPAACompliantAdmin{
+   
+	private Integer employeeID;
+    private String role;
+    private ArrayList<String> securityIncidents=new ArrayList<String>();
+    
+	public AdminUser(Integer id,String role) {
+		super(id);
+		setRole(role);
+	}
+
+	@Override
+	public ArrayList<String> reportSecurityIncidents() {
+		// TODO Auto-generated method stub
+		return securityIncidents;
+	}
+
+	@Override
+	public boolean assignPin(int pin) {
+		   if (pin < 100000) {
+	            return false;
+	        }
+	        this.setPin(pin);
+	        return true;
+	}
+
+	@Override
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+	      if (!this.getId().equals(confirmedAuthID)) {
+	            authIncident();
+	            return false;
+	        }
+	        return true;
+	    }
+
+	  public void newIncident(String notes) {
+	        String report = String.format(
+	            "Datetime Submitted: %s \n,  Reported By ID: %s\n Notes: %s \n", 
+	            new Date(), this.id, notes
+	        );
+	        securityIncidents.add(report);
+	    }
+	    public void authIncident() {
+	        String report = String.format(
+	            "Datetime Submitted: %s \n,  ID: %s\n Notes: %s \n", 
+	            new Date(), this.id, "AUTHORIZATION ATTEMPT FAILED FOR THIS USER"
+	        );
+	        securityIncidents.add(report);
+	    }
+	
+	public Integer getEmployeeID() {
+		return employeeID;
+	}
+
+	public void setEmployeeID(Integer employeeID) {
+		this.employeeID = employeeID;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public ArrayList<String> getSecurityIncidents() {
+		return securityIncidents;
+	}
+
+	public void setSecurityIncidents(ArrayList<String> securityIncidents) {
+		this.securityIncidents = securityIncidents;
+	}      
+	
+   
+    
+    
+}
